8903de68031be5e1e51d5e841d46236b67624fbe
Andrew844 <39277831+Andrew844@users.noreply.github.com>
2020-11-01 22:51:11 +0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyCngiKCG47zbOMCDkmC45vT00gT2TuJ31g',
+  fbDbUrl: 'https://angular-blog-cbac7.firebaseio.com'
 };
